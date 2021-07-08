c84ce9655370e8ad62c7985bd7ed7868b8113b07
kimshape <elves9918@gmail.com>
2021-07-08 11:55:58 +0900

    210708 마지막커밋

--- 물결전환효과/물결/app.js
+++ 물결전환효과/물결/app.js
@@ -1,6 +1,6 @@
-// import{Ripple} from '../../물결/ripple.js';
-// import{Dot} from '../../물결/dot.js';
-// import{collide} from '../../물결/utils.js'
+import {Ripple} from '../물결/ripple.js';
+import {Dot} from '../물결/dot.js';
+import {collide, getBWValue} from '../물결/utils.js'
 
 class App{
     constructor(){
@@ -18,8 +18,8 @@ class App{
         window.addEventListener('resize',this.resize.bind(this),false);
         this.resize();
 
-        this.radius=10;
-        this.pixelSize=30;
+        this.radius=16;
+        this.pixelSize=16;
         this.dots = [];
 
         this.isLoaded =false;
@@ -27,11 +27,11 @@ class App{
             x:0,
             y:0,
             width:0,
-            height:0,
+            height:0
         };
 
         this.image=new Image();
-        this.image.src='dropthebit_logo.png'
+        this.image.src='masterpiece-4359011_1920.jpg'
         this.image.onload=()=>{
             this.isLoaded=true;
             this.drawImage();
@@ -49,7 +49,7 @@ class App{
 
         this.canvas.width=this.stageWidth*this.pixelRatio;
         this.canvas.height=this.stageHeight*this.pixelRatio;
-        this.ctx.scale(this.pixelRatio,this.pixelRatio);
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
 
         this.tmpcanvas.width =this.stageWidth;
         this.tmpcanvas.height=this.stageHeight;
@@ -101,7 +101,7 @@ class App{
         this.imgPos.width, this.imgPos.height
     );
     
-    this.imgData = this.tmpctx.getImageData(0,0, this. stageWidth, this. stageHeight);
+    this.imgData = this.tmpctx.getImageData(0,0, this.stageWidth, this.stageHeight);
 
  this.drawDots();
     }
@@ -124,22 +124,28 @@ class App{
                 const red = this.imgData.data[pixelIndex+0];
                 const green = this.imgData.data[pixelIndex+1];
                 const blue = this.imgData.data[pixelIndex+2];
-
+                const scale = getBWValue(red,green,blue,false);
                 const dot = new Dot(
                     x,y,
                     this.radius,
                     this.pixelSize,
-                    red,green,blue
+                    red,green,blue,
+                    scale
                 );
+                if(dot.targetRadius>0.1){
                     this.dots.push(dot);
+                }
             }
+
         }
     }
 
 
-animate(){
+
+    animate(){
     window.requestAnimationFrame(this.animate.bind(this));
-    this.ripple.animate(this.ctx);
+    
+    this.ripple.animate();
 
     for(let i=0; i<this.dots.length; i++){
         const dot = this.dots[i];
@@ -156,13 +162,10 @@ animate(){
 onClick(e){
     this.ctx.clearRect(0,0,this.stageWidth,this.stageHeight);
     
-    this.ctx.drawImage(
-        this.image,
-        0,0,
-        this.image.width, this.image.height,
-        this.imgPos.x, this.imgPos.y,
-        this.imgPos.width, this.imgPos.height
-        );
+    for(let i = 0; i<this.dots.length; i++){
+        this.dots[i].reset();
+    }
+
 
     this.ripple.start(e.offsetX,e.offsetY);
 }
@@ -170,103 +173,6 @@ onClick(e){
 }
 
 
-
-class Dot {
-    constructor(x,y,radius,pixelSize,red,green,blue){
-        this.x=x;
-        this.y=y;
-        this.radius=radius;
-        this.pixelSize=pixelSize;
-        this.pixelSizeHalf=pixelSize/2;
-        this.red=red;
-        this.green=green;
-        this.blue=blue;
-        this.canvas=document.createElement('canvas');
-        document.body.appendChild(this.canvas);
-        this.ctx=this.canvas.getContext('2d');
-        
-    }
-
-    animate(ctx){
-        ctx.beginPath();
-        ctx.fillStyle = '#000';
-        ctx.fillRect(
-            this.x-this.pixelSizeHalf,
-            this.y-this.pixelSizeHalf,
-            this.pixelSize,this.pixelSize
-        );
-        const PI2 = Math.PI*2;
-        ctx.beginPath();
-        ctx.fillStyle=`rgb(${this.red}, ${this.green}, ${this.blue})`;
-        ctx.arc(this.x, this.y, this.radius, 0, PI2, false);
-        ctx.fill();
-    }
-
-    reset(){
-
-    }
-
-}
-class Ripple{
-    constructor(){
-        this.x=0;
-        this.y=0;
-        this.radius=0;
-        this.maxRadius=0;
-        this.speed=10;
-        this.canvas=document.createElement('canvas');
-        document.body.appendChild(this.canvas);
-        this.ctx=this.canvas.getContext('2d');
-
-        this.tmpcanvas = document.createElement('canvas');
-        this.tmpctx=this.tmpcanvas.getContext('2d');
-    }
-    resize(stageWidth,stageHeight){
-        this.stageWidth=stageWidth;
-        this.stageHeight=stageHeight;
-    }
-    start(x,y){
-        this.x=x;
-        this.y=y;
-        this.radius=0;
-        this.maxrRadius=this.getMax(x,y);
-    }
-
-    animate(){
-        if(this.radius<this.maxRadius){
-            this.radius+=this.speed;
-        }
-
-        this.ctx.beginPath();
-        this.ctx.fillStyle='#00ff00';
-        this.ctx.arc(this.x, this.y, this.radius,Math.PI*2,false)
-        this.ctx.fill();
-    }
-    getMax(x,y){
-        const c1 = distance(0,0,x,y);
-        const c2 = distance(this.stageWidth,0,x,y);
-        const c3 = distance(0,this.stageHeight,x,y);
-        const c4 = distance(this.stageWidth,this.stageHeight,x,y);
-        return Math.max(c1,c2,c3,c4);
-        
-    }
-}
-
-
-function distance(x1,y1,x2,y2){
-    const x =x2-x1;
-    const y =y2-y1;
-    return Math.sqrt(x*x+y*y);
-
-}
-function collide(x1,y1,x2,y2,radius){
-    if (distance(x1,y1,x2,y2)<=radius){
-        return true;
-
-    }else{
-        return false;
-    }
-}
 window.onload=()=>{
     new App();
 }
